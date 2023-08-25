4d307510d1cc19f17403368715ecb34fb729342d
santhosh-natarajan <santhosh.swengineer@gmail.com>
2023-08-25 14:46:26 +0530

    updated

--- src/app/flash-screen/flash-screen.component.ts
+++ src/app/flash-screen/flash-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-flash-screen',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlashScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
+    setTimeout(() => { this._router.navigate(['/form-view']) }, 2000)
   }
 
 }
--- src/app/form-view/form-view.component.ts
+++ src/app/form-view/form-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-view',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  onClickBack() {
+      return this._router.navigate(['/'])
   }
 
 }
